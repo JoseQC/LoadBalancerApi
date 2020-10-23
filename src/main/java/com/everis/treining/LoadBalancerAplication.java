2448cd2132777eed87b6cbcac2c1e65898ee126c
joseqc28 <jossukeqc@gmail.com>
2020-10-22 22:23:57 -0500

    first commit

---
+++ src/main/java/com/everis/treining/LoadBalancerAplication.java
@@ -0,0 +1,19 @@
+package com.everis.treining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableZuulProxy
+@EnableConfigServer
+public class LoadBalancerAplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoadBalancerAplication.class, args);
+	}
+
+}
